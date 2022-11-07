c5cbf485da52b50744b5f5beb9cd606724deaf09
Wan Zulkarnain <wanzulkarnain69@gmail.com>
2022-11-07 16:43:11 +0800

    Add log

--- index.js
+++ index.js
@@ -16,12 +16,10 @@ const server = http.createServer((request, response) => {
       
       var signature = request.headers['x-signature'];
 
-      console.log('ini signature: ' + signature);
       var publicKey = '-----BEGIN PUBLIC KEY-----\nMIIBojANBgkqhkiG9w0BAQEFAAOCAY8AMIIBigKCAYEA1P+PnZyqguEJuxgGKEwY\nVvT3ERcDjfV5P+W/N4pN7dWZHLlunsB0kAy6PD7/K+QaOqWx3fezty+M/dMUDlMR\njYJo5OF0mnkR1YcRhTcHL4jf0QECB7eAYHXwguZlUWzvLQLRqN3bT6qsiOEJzxVE\nphuC8A5OnF1GlGQiHlALOAy9qUHB3uC4sJZUIqLPqilTOO+QePapFDgJHz8aY7D8\nFBQxTFfAG+r0pNNMRgdpT6VxZAxVPmtiD7VCpfvNo2/lvt0fDlxPM03EtNSvA6G4\nDt6+8QDhzDiIOI38b0+HkPCpXoVpgA10liTL3OjDfZuGcLOBKxA3NQPMp/oGj1SS\nNwlyJpDLjizQZSqaGx5hW7Qq65/iOpr2qllR1wIRGWO5OtpzQM7VSOLOYidJOFz6\nA3HItnadtAcz7Ek6o7Sva0kMIbYlqbWwLjAUWOSxW27dvk1KS5LJocUSbCeaa9T4\nAPjh5kJMX/RDrUMEGOMjJFc33RP6Hpsi7ORU9AcaS8TbAgMBAAE=\n-----END PUBLIC KEY-----\n';
-      console.log('ini body: ' + body);
-
+ 
       var result = chipp.verify(data, Buffer.from(signature, 'base64'), publicKey);
-      console.log('Verification: ' + result);
+      console.log('This verification value will indicate status of validity: ' + result);
     })
 
     request.on('end', function() {
