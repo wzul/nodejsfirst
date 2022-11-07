551d7dbdff32e5a4ac361d20f6a68125dc49110d
Wan Zulkarnain <wanzulkarnain69@gmail.com>
2022-11-07 12:06:15 +0800

    Add net

--- index.js
+++ index.js
@@ -1,9 +1,36 @@
 const http = require('http');
 
-const server = http.createServer((req, res) => {
-  res.end('Hello World\n');
+const server = http.createServer((request, response) => {
+  if (request.method == 'POST') {
+    console.log('POST')
+    var body = ''
+    request.on('data', function(data) {
+      body += data
+      console.log('Partial body: ' + body)
+    })
+    request.on('end', function() {
+      console.log('Body: ' + body)
+      console.log('Header: ' + JSON.stringify(request.headers));
+      response.writeHead(200, {'Content-Type': 'text/html'})
+      response.end('post received')
+    })
+  } else {
+    console.log('GET')
+    var html = `
+            <html>
+                <body>
+                    <form method="post" action="http://www.sendiri.com:4242">Name: 
+                        <input type="text" name="name" />
+                        <input type="submit" value="Submit" />
+                    </form>
+                </body>
+            </html>`
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    response.end(html);
+  }
 });
 
-server.listen(process.env.PORT || 80, () => {
+server.listen(process.env.PORT || 4242, () => {
   console.log('Server is running...');
 });
+
