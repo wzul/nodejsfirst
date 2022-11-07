9085d7edb6b1536c67e9c6b4d036ca4bd58c845d
Wan Zulkarnain <wanzulkarnain69@gmail.com>
2022-11-07 16:44:58 +0800

    expecting false

--- index.js
+++ index.js
@@ -14,11 +14,12 @@ const server = http.createServer((request, response) => {
     request.on('data', function(data) {
       body+=data;
       
-      var signature = request.headers['x-signature'];
-
+      // var signature = request.headers['x-signature'];
+      var signature = 'ff';
       var publicKey = '-----BEGIN PUBLIC KEY-----\nMIIBojANBgkqhkiG9w0BAQEFAAOCAY8AMIIBigKCAYEA1P+PnZyqguEJuxgGKEwY\nVvT3ERcDjfV5P+W/N4pN7dWZHLlunsB0kAy6PD7/K+QaOqWx3fezty+M/dMUDlMR\njYJo5OF0mnkR1YcRhTcHL4jf0QECB7eAYHXwguZlUWzvLQLRqN3bT6qsiOEJzxVE\nphuC8A5OnF1GlGQiHlALOAy9qUHB3uC4sJZUIqLPqilTOO+QePapFDgJHz8aY7D8\nFBQxTFfAG+r0pNNMRgdpT6VxZAxVPmtiD7VCpfvNo2/lvt0fDlxPM03EtNSvA6G4\nDt6+8QDhzDiIOI38b0+HkPCpXoVpgA10liTL3OjDfZuGcLOBKxA3NQPMp/oGj1SS\nNwlyJpDLjizQZSqaGx5hW7Qq65/iOpr2qllR1wIRGWO5OtpzQM7VSOLOYidJOFz6\nA3HItnadtAcz7Ek6o7Sva0kMIbYlqbWwLjAUWOSxW27dvk1KS5LJocUSbCeaa9T4\nAPjh5kJMX/RDrUMEGOMjJFc33RP6Hpsi7ORU9AcaS8TbAgMBAAE=\n-----END PUBLIC KEY-----\n';
  
       var result = chipp.verify(data, Buffer.from(signature, 'base64'), publicKey);
+      console.log('we expect this to be false');
       console.log('This verification value will indicate status of validity: ' + result);
     })
 
